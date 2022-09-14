37172bfc7224dc96b9cc57af42879c3ab1c5d17f
Fabiano Franca <fabianofranca.ti@gmail.com>
2022-09-14 11:57:28 -0300

    Estrutura do banco de dados criada.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,35 @@
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+//HTTP methods / API RESTful / HTTP Codes
+
+/**
+ * Query: São utilizados quando precisamos persistir estados (filtros, ordenação, paginação, envio de informações não sensíveis etc). Parâmetros da url nomeados.
+ * http://localhost:3333/ads?page=2&sort=title
+ * 
+ * Route: Parâmetros da url não nomeados. São utilizados quando queremos fazer uma identificação de um recurso.
+ * http://localhost:3333/ads/5
+ * 
+ * Body: São utilizados quando queremos enviar várias informações, geralmente usados no envio de formulários, onde temos dados sensíveis. 
+ *      Parâmentros que não ficam na url, não são visíveis.
+ *  http://localhost:3333/games
+ * {
+ * id: 1,
+ * title: 'Apex'
+ * bannerUrl: 'c:/user/teste/documentos/esports/gamesImgs'
+ * }
+ */
+
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', () => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
   //return response.send('Acessou Ads!')
   return response.json([
     {id: 1, name: 'Anúncio 1'},
@@ -10,7 +37,12 @@ app.get('/ads', (request, response) => {
     {id: 3, name: 'Anúncio 3'},
     {id: 4, name: 'Anúncio 4'},
   ])
-})
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  //const adId = resquest.params.id;
+  return response.json([])
+});
 
 
 //localhost:3333/
